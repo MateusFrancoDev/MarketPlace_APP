9c39450b7ce1f6069123ccd308b0a7dc1b44b343
MateusFrancoDev <mateusfranc2@gmail.com>
2024-04-20 00:01:13 -0300

    NavBar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/common/header";
 import { Container } from "./styled";
+import NavBar from "../../components/common/navbar";
 
 const Home = () => {
   return (
     <Container>
      <Header />
+     <NavBar />
     </Container>
   );
 }
